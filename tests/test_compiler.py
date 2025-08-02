2b4cfa5bc5e02772ac519e5f2734c381f01992de
yuki161022 <yuki161022@gmail.com>
2025-08-03 00:00:30 +0900

    Update test_compiler.py

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -6,6 +6,11 @@ def analyze_text(full_text):
     text_line = full_text.splitlines() 
 
     for line in text_line:
+        #空白の行がないかの確認
+        line = line.strip()
+        if not line:
+            continue
+        #その行がコマンドかテキスト文かの判別
         if line[0] =='\\':
             analyzed_list.append({'type': 'command','text': line })
         else:
