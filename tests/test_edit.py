22aee245b630109ad809a2d6d12c3a11c9e89ed2
sagami1804 <61611865+sagami1804@users.noreply.github.com>
2025-08-03 02:07:03 +0900

    Update test_edit.py

--- tests/test_edit.py
+++ tests/test_edit.py
@@ -8,14 +8,18 @@ def image(**kwargs):
         return ImageClip(path)
     except Exception as e:
         return None
+    
 #セクションタイトル（テロップ）を生成
 def section(**kwargs):
     text = kwargs.get('text', ' ')
     duration = float(kwargs.get('duration', 3))
+    clip = TextClip(text=text, font_size=70, color='white', font="fonts/Corporate-Logo-Rounded-Bold-ver3.otf").with_duration(duration)
+    return clip
 
-    return TextClip(text, fontsize=70, color='white', font='Arial-Bold').with_duratio(duration)
 #無音の「音声クリップ」を生成
 def delay(**kwargs):
     duration = float(kwargs.get('duration', 1))
     
     return AudioClip(lambda t: 0, duration=duration)
+
+section(text="セクションタイトル", duration=3)
