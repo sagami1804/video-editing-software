813563da1697d8099f7156ddebe60af1be0f4080
yuki161022 <yuki161022@gmail.com>
2025-08-02 23:41:08 +0900

    Update test_compiler.py

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -1,12 +1,12 @@
+import re
+
 def analyze_text(full_text):
     analyzed_list = []
 
     text_line = full_text.splitlines() 
 
     for line in text_line:
-        if line[0] == '\\':
-            
-        else:
+        if line[0] !='\\':
             analyzed_list.append({'type': 'text', 'text': line})
         
     return analyzed_list
